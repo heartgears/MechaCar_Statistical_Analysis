21abda665e0493193f8a7957a06c1ee0ef942eba
Hunter Gierhart <102868909+heartgears@users.noreply.github.com>
2022-07-24 14:40:07 -0400

    Updating final, running code

--- Challenge/MechaCarChallenge.RScript.R
+++ Challenge/MechaCarChallenge.RScript.R
@@ -1,16 +1,13 @@
 # Read in Dependencies
 
-install.packages("plyr") 
-install.packages("dplyr") 
-library(dbplyr)
+library(dplyr)
 library(plyr)
-library("tidyverse")
+library(tidyverse)
 
 mpg <- read.csv(file="/Users/hunter/Desktop/Desktop - Hunter’s iMac/Columbia-Bootcamp/Mod_15_R/R_Analysis/Challenge/Resources/MechaCar_mpg.csv")
 suspension <- read.csv(file="/Users/hunter/Desktop/Desktop - Hunter’s iMac/Columbia-Bootcamp/Mod_15_R/R_Analysis/Challenge/Resources/Suspension_Coil.csv")
 
 # Deliverable 1
-
 lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle 
    + ground_clearance + AWD, data = mpg)
 
@@ -18,19 +15,26 @@ summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle
            + ground_clearance + AWD, data = mpg))
 
 # Deliverable 2
-
 suspension_data <- (suspension %>%
       summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)))
 
 total_summary <- data.frame(suspension_data)
 total_summary
 
-#lot_summary_demo <- (suspension %>% group_by(Manufacturing_Lot))
-#lot_summary <- summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
-#lot_summary
+# Create the Summary By Lot
+lot_summary_data <- suspension %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+lot_summary <- data.frame(lot_summary_data)
+lot_summary
 
-lot_summary_test <- (suspension %>% group_by(Manufacturing_Lot) %>%
-      + summarize(Mean=mean(suspension$PSI),Median=median(suspension$PSI),
-                  Variance=var(suspension$PSI),SD=sd(suspension$PSI), .groups='keep'))
-lot_summary_test
+# Deliverable 3
+t.test(suspension$PSI,mu=1500)
+
+lot1 <- subset(suspension, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
 
